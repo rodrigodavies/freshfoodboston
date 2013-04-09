bd24751843c351558c8041cce27cb307df655837
Rodrigo Davies <rodrigo.davies@gmail.com>
2013-04-09 01:12:43 -0400

    Removes surplus themes

--- wp-content/themes/TheColumnist/footer.php
+++
@@ -1,6 +0,0 @@
-<div id="footer" role="contentinfo">
-    <p><a href="<?php echo get_option('home'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></p>
-    <p>A theme designed by <a href="http://benmartineau.com/" target="_blank" title="Ben Martineau">Ben Martineau</a></p>
-    <p><a href="<?php bloginfo('rss2_url'); ?>">Subscribe RSS</a></p>
-</div>
-
--- wp-content/themes/birdtips/js/birdtips.js
+++
@@ -1,9 +0,0 @@
-jQuery(function(){
-	
-	/* toggle small menu */
-	jQuery("#small-menu").click( function() {
-		jQuery(".menu").slideToggle();
-		jQuery(this).toggleClass("active");
-	});
-
-});
