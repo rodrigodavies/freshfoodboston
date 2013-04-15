abe9d09f271dcc085ca573bd58bd800b922830bf
Rodrigo Davies <rodrigo.davies@gmail.com>
2013-04-15 12:19:15 -0400

    Scales front page prompt box for mobile
    - Hero box added via front-end was not scaling fonts on mobile
    - Shrinks margins for mobile front page
    - Moves design credits into About Us

--- wp-content/themes/mixfolio/footer.php
+++ wp-content/themes/mixfolio/footer.php
@@ -14,7 +14,7 @@
 		<footer id="colophon" class="row" role="contentinfo">
 			<div id="site-generator" class="twelve columns">
 				<span class="right">
-					<?php printf( __( 'Based on %1$s by %2$s', 'mixfolio' ), '<a href="http://graphpaperpress.com/themes/mixfolio/">Mixfolio</a>', '<a href="http://graphpaperpress.com/" rel="designer">Graph Paper Press</a>' ); ?>. <a href="http://thenounproject.com/noun/fruit/#icon-No3378" target="_blank">Fruit</a> designed by <a href="http://thenounproject.com/Jaymepayme" target="_blank">Jayme Davis</a> from The Noun Project
+					Built by <a href="about-us/">Fresh Food Boston</a>, 2013.
 				</span><!-- .right -->
 			</div><!-- #site-generator -->
 		</footer><!-- #colophon -->
